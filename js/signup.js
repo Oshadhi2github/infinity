aa8b94d3e5864110afc4905eb4bea47417e6ba13
Oshadhi2github <pabasarasgck@gmail.com>
2022-11-06 01:35:41 +0530

    12th commit

--- js/signup.js
+++ js/signup.js
@@ -15,7 +15,8 @@ function User(username,email,type,avatar,password){
 const registerUser=()=>{
     //trim=> "__ABC_"==> after trim ==> "ABC" (spaces rid)
     let tempPassword = $('#password').val().toString().trim();
-    let encryptedPwd = CryptoJS.AES.encrypt('infinity',tempPassword);
+    console.log(tempPassword)
+    let encryptedPwd = CryptoJS.AES.encrypt(tempPassword,'infinity');
     let user = new User(
     $('#username').val().toString().trim(),
         $('#email').val().toString().trim(),
