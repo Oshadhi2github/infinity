b8eeec9eb29de9b2fba47b34b755f6115964c424
Oshadhi2github <pabasarasgck@gmail.com>
2022-11-25 11:29:36 +0530

    in middle box add to onclick and place order process in dashoard js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,21 @@
+
+//=======================
+let itemsForOrder=[];
+//=======================
+
 let temp=JSON.parse(localStorage.getItem('users'));
 console.log(temp)
 if (temp===null){
     window.location.replace('index.html');
 }
+
+const placeOrder=(item)=>{
+    itemsForOrder.push(item);
+
+    itemsForOrder.forEach(data=>{
+        let row= <li>${data}</li>;
+        $('items-list').innerHTML=row;
+    });
+}
+
+
